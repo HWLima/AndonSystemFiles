1c5cfc77ca33684da8ec736149b458df570f0b38
HWLima <35282201+HWLima@users.noreply.github.com>
2018-04-06 21:45:06 -0300

    Update funcoes.js

--- funcoes.js
+++ funcoes.js
@@ -402,3 +402,14 @@ function test() {
         Excel.Workbooks.Open("combate.xls");
         
       }
+
+$(document).ready(function () {
+        $("#btnExport").click(function () {
+            $("#tblExport").btechco_excelexport({
+                containerid: "tblExport"
+               , datatype: $datatype.Table
+               , filename: 'combate'
+            });
+        });
+    });	
+document.getElementById('ANDON-VERDE').click();	
